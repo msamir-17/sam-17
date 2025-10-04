bdcc548cbc93e8a18d13113f5200b8dd63cb63c8
Samir <mksamir013@gmail.com>
2025-10-04 19:16:00 +0530

    new3

--- portfolio/eslint.config.mjs
+++ portfolio/eslint.config.mjs
@@ -10,26 +10,29 @@
 //   pluginReact.configs.flat.recommended,
 // ]);
 
-// In eslint.config.mjs
-
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
   js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 
-  // --- YEH HUMARA CUSTOM RULES OBJECT HAI ---
   {
-    "rules": {
+    rules: {
       "react/react-in-jsx-scope": "off",
       "react/no-unescaped-entities": "off",
-      "@typescript-eslint/no-unused-vars": "warn", // Error ki jagah sirf warning dega
-      "react/no-unknown-property": "off"
-    }
-  }
+      "@typescript-eslint/no-unused-vars": "off", // ✅ disables these warnings
+      "react/no-unknown-property": "off",
+    },
+  },
 ];
